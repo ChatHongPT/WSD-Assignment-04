dc951a84e3f808dbac3acc976b9eaaf9702d6679
suk924600 <suk924600@naver.com>
2024-12-26 00:52:13 +0900

    Update: deploy.yml

---
+++ src/script/movie/URL.d.ts
@@ -0,0 +1,5 @@
+declare module '@/script/movie/URL' {
+    const URL: any; // 적절한 타입으로 교체
+    export default URL;
+  }
+  
---
+++ src/script/movie/wishlist.d.ts
@@ -0,0 +1,5 @@
+declare module '@/script/movie/wishlist' {
+    const wishlist: any; // 적절한 타입으로 교체
+    export default wishlist;
+  }
+  
