bbc39b5296534e01879a17e7f896cf18e218d3a8
suk924600 <suk924600@naver.com>
2024-12-26 15:38:38 +0900

    Update: Workflow 수정

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,4 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
-// 카카오 JavaScript SDK 초기화
-window.Kakao.init(import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY)
-
 createApp(App).use(router).mount('#app')
