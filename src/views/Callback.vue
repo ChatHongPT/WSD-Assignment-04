f17bb7ae11ff8e50ddda6d183ed081a8ef35d257
suk924600 <suk924600@naver.com>
2024-12-26 20:03:59 +0900

    Update: BASE_URI 수정

--- src/views/Callback.vue
+++ src/views/Callback.vue
@@ -20,7 +20,7 @@
         const data = {
           grant_type: "authorization_code",
           client_id: import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY, // JavaScript API 키
-          redirect_uri: '/kakaologin/callback', // 리디렉션 URI
+          redirect_uri: `${window.location.origin}/WSD-Assignment-04/kakaologin/callback`, // 리디렉션 URI
           code: code
         };
   
